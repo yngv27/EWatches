759e042ce3e5da73c2b3c1354bd5845c68a64d9d
JV <61598758+yngv27@users.noreply.github.com>
2022-07-19 17:06:11 -0400

    Update i2ctest.js
    version without delay() which hangs it pretty often. Also easy to resume after crash

--- tools/i2ctest.js
+++ tools/i2ctest.js
@@ -27,8 +27,57 @@ function detect(i2c,first, last) {
 
 // you can use SW I2C, or use a built-in version like I2C1
 myI2C = new I2C();
-//myI2C = I2C1;
+myI2C = I2C1;
 
+var SDA=0, SCL=0, EN=0;
+
+function getNextPin(idx) {
+  for(let c=idx; c<pins.length && pinBusy[c] ; c++);
+  return c >= pins.length ? -1 : c;
+}
+
+function test() {
+  resetBusy();
+  SDA = getNextPin(SDA);
+  pinBusy[SDA] = true;
+  SCL = getNextPin(SCL);
+  pinBusy[SCL] = true;
+  EN = getNextPin(EN);
+  if(SCL != 3) {
+    
+    print(`SDA=${SDA}, SCL=${SCL}, EN=${EN} `);
+    pins[EN].set();
+    myI2C.setup( {scl: pins[SCL], sda: pins[SDA], bitrate: 100000} );
+    let res = detect( myI2C );
+    if(res.length > 0) {
+      print(`Scanning SCL=${pins[SCL]} SDA=${pins[SDA]}`);
+      print(`--------------- RES= ${res}--------------`);
+    }
+    
+  }
+  
+  EN=getNextPin(++EN);
+  if(EN == -1) {
+    pinBusy[SCL++]=false;
+    SCL=getNextPin(SCL);
+    if(SCL == -1) {
+      pinBusy[SDA++]=false;
+      SDA=getNextPin(SDA);
+      if(SDA == -1) {
+        print("DONE");
+        return;
+        SDA=0;
+      }
+      SCL=0;
+    }
+    EN=0;
+  }
+}
+
+t=test;
+s=setInterval;
+
+/* older version with delay() which seems to gum things up
 for(let EN=0; EN < pins.length; EN++) {
   resetBusy();
   pinBusy[EN] = true;
@@ -55,3 +104,4 @@ for(let EN=0; EN < pins.length; EN++) {
    // comment out below 2 lines is you don't need to ENable a pin to use I2C 
   pinBusy[EN] = false;
 }
+*/
