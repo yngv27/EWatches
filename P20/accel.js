164261c2a038b729fb0026091f0cfaa2f0597abf
JV <61598758+yngv27@users.noreply.github.com>
2022-07-28 13:29:29 -0400

    Update accel.js
    tweaked for better 'faceup' response

--- P20/accel.js
+++ P20/accel.js
@@ -21,23 +21,27 @@ var ACCEL = {
     ACCEL.I2C.setup({scl:D16,sda:D15,bitrate:200000});
 
     var id = ACCEL.readBytes(0x0F,1)[0];
-    ACCEL.writeByte(0x20,0x47);
+    ACCEL.writeByte(0x20,0x4f);
     ACCEL.writeByte(0x21,0x00); //highpass filter disabled
     ACCEL.writeByte(0x22,0x40); //interrupt to INT1
-    ACCEL.writeByte(0x23,0x88); //BDU,MSB at high addr, HR
+    ACCEL.writeByte(0x23,0x80); //BDU,MSB at high addr, HR
     ACCEL.writeByte(0x24,0x00); //latched interrupt off
-    ACCEL.writeByte(0x32,0x18); //threshold = 250 milli g's
-    ACCEL.writeByte(0x33,0x01); //duration = 1 * 20ms
-    ACCEL.writeByte(0x30,0x08); //YH interrupt 
+    ACCEL.writeByte(0x32,0x30); //threshold = 250 milli g's
+    ACCEL.writeByte(0x33,0x02); //duration = 1 * 20ms
+    ACCEL.writeByte(0x30,0x02); //XH interrupt 
     pinMode(ACCEL.pin,"input",false);
     setWatch(()=>{
-       var  v = ACCEL.read();
-       if (Math.abs(v.x)<2000 && v.y>250 && v.z>0) {
-        if (wOS.awake)
-           wOS.time_left = wOS.ON_TIME; //reset LCD on time.
-        else
-           ACCEL.emit("faceup");
-       }
+      print("ACCEL INT");
+      setTimeout(()=>{
+        var  xyz = ACCEL.read();
+        print(`v=${ACCEL.readBytes(0,1)[0]} xyz=${JSON.stringify(xyz)}`);
+    if(xyz.x > 400 && xyz.x < 800 && xyz.y < 300 && xyz.y > -200 && xyz.z > 0) {
+          if (wOS.awake)
+            wOS.time_left = wOS.ON_TIME; //reset LCD on time.
+          else
+            ACCEL.emit("faceup");
+        }
+      }, 300);
     },ACCEL.pin,{repeat:true,edge:"rising",debounce:50});
     return id;
   },
