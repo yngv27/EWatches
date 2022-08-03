981c7b4e7c630f4f897133c7c0fd5549067ce30b
JV <61598758+yngv27@users.noreply.github.com>
2022-08-03 16:47:12 -0400

    Create dotmatrix.js

---
+++ GW32/dotmatrix.js
@@ -0,0 +1,268 @@
+Modules.addCached("FontHaxorNarrow7x17",function(){var a=atob("AAAAAAAAAAAB/MAAAJAAcAAAABIADgAAAAACAAngB4AeeAHgB5AAQAAAAHEAREAiID/4CIgERAEcAAAAwMCQgEmAGTADJAISBgYAAAAHgHQgRBAjCA5kAAwAPQAAAkABwAAAAAfADBgIAggAgAACACCAIDBgB8AAAAEgAGAA/AAYABIAAAABAACAAEAB/AAQAAgABAAAAAAkABwAAAAgABAACAAEAAIAAAAADAAGAAAAAIABgAMABgAMABgAAAAB/AEBAQxAjCAgIA/gAAACAAIAAgAB/8AAACBwIMgQhAiCA4EAAACAgIAgQhAhCA94AAAAGAA0ACIAYQB/8ABAAAAPhARBAiCBEECHwAAAH/ARBAiCBEEBHwAAAIAAQPAhgBMADgAAAAHvAQhAhCBCEB7wAAAHhAQhAhCBCEB/wAAADDAGGAAAAYUAwwAAAAQABQAEQAQQBAQAAABEACIAEQAIgARAAiAAAAICAIIAIgAKAAIAAAAIAAgABBMCEAEQAHAAAAAf8BAECOIEiQJEgP4AAAAP8BiAEEAGIAD/AAAB/8CEIEIQIQgPeAAAA/4CAIEAQIAgICAAAB/8CAIEAQEBAH8AAAB/8CEIEIQIQgQBAAAB/8CIAEQAIgAQAAAAA/4CAIEAQIQgI+AAAB/8AEAAIAAQAf/AAABAED/4EAQAAAQGAgCBAECAIH/gAAAf/ABAAFAAxgGAwAAAf/AACAAEAAIAAQAAAf/AYAAMABgAH/wAAAf/AYAAMAAGAH/wAAAP+AgCBAECAID/gAAAf/AhABCACEADwAAAAP+AgCBAUCAQD/QAAAf/AhABCACHADxwAAAOCAiCBCECCICDgAAAQAAgAB/8CAAEAAAAAf+AACAAEAAIH/gAAAfAAB4AAMAHgHwAAAAf8AAGAAwAGAADAABgf8AAABgcAzAAYADMAYHAAABwAAYAAPwBgAcAAAABAMCBoEcQLAgYBAAAD/8EAIIAQAAAYAAMAAGAADAABgAAgAABACCAEH/4AAABAAMAAgAAwAAQAAAAAAgABAACAAEAAIAAQAABAABAABAAAAAATgBIgCRAEkAH8AAAH/wAhACBAECAH4AAAAfgBAgCBAECAEIAAAAfgBAgCBACEB/8AAAAfgBIgCRAEiAHIAAAAgAH/gSAAkABIAAAAAdYBFICKQEUgPJAQMAAAP/gBAAEAAIAAP4AAAAAgCBAn+AAEAAIAAAAAQAAQAAgIBCf8AAAP/gAIAAwAGQAQYAAAAAgQBA/+AAEAAIAAAB/gCAAH+AIAAP4AAAB/gBAAEAAIAAP4AAAA/ACBAECAIEAPwAAAB/4BCAECAIEAPwAAAA/ACBAECAEIAf+AAAB/gBAAEAAIAAIAAAAAxACRAEiAIkAIwAAABAAf+AECAIEAQIAAAB/AABAACAAIAf4AAABwAAcAAGABwAcAAAAB+AADAA4AAMAfgAAABhgAkAAwACQAYYAAAB4MANgAEAAwAeAAAABBgCFAEyAKEAYIAAAOTgjYhABAAAD/4AAAQAQjYg5OAAAAEAAQAAgAAgAAgABAAEAAAAB/gMQAggAxDAf5AACAAAAQB/+AEEAQIAAQAAgAAAXQARABBACCAEEAEQAXQAAAH/4AAQoAhgBAACAAABwQEQQoQiQRAQcAAADYwJIQRIghJAxsAAABwQkQQoQiQRAQcAAABwQEQSIQ0QRQQcAAACAAkAAv/iQAAgAAAAAQYAhQFMgShAGCAAAAQYEhQFMgShAGCAAAAQYAhQFMgChAGCAAADAAJAASAAYAAAAAE4ASKAkaBJEB/AAAABECEIH/wBAgEBAAABAAEAAAAAAAgQBA/+FAEMAIAAAAxAKRAkiAokAIwAAAQAAQABAAAAAAMQEkQFIgSJACMAAAAMQAkSBI0CJQCMAAAAQAH/gBAgCBBUCDAAAAAAgwBCgKZAlCAMEAAAAgwJCgKZAlCAMEAAAAgwBCgKZAFCAMEAAAH/wIQBQgEhwA8cAAAA/wGIBQQEYgAP8AAAA/wmICQQCYgAP8AAAA/xGIBQQEYgAP8AAAA/wmIAQQCYgAP8AAAX/xAAgABAACAAEAAAD/gIAhQBEgCAgIAAAD/gIAkQBogCggIAAAD/hIAhQBEgCAgIAAAH/wIQhQhEhCBAEAAAH/wIQoQhohCRAEAAAH/woQgQhChCBAEAAAH/xIQhQhEhCBAEAAAEAQv/iQBAAAFAET/4UAQAAAf/EgCFAERAQB/AAAAf/AhCBCEBAQB/AAAAf/AYAEMAQGAH/wAAAf/EYAEMAQGAH/wAAAP+AgCFAESAID/gAAAP+CgCJAEKAID/gAAAP+CgCJAEKAIj/gAAAP+CgCBAEKAID/gAAAf/EhAFCASHADxwAAAf+CACKAEIAIH/gAAAf+AACEAEQAIH/gAAAf+CACIAEIAIn/gAAAf+CACAAEIAIH/gAAAcAAGAED8QYAHAAAAAQAAgAR//CAAEAAAAAP/AgABEECIIDoQAPAAAAH+AEABQAEgAAgAAAACcAJEBSIEkgA/gAAACcApECSICkgA/gAAACcBJEBSIEkgA/gAAACcApEASICkgA/gAAAACFAET/4AAQAAgAAAD8AIEBQIEgQAhAAAAD8AIEAQOAgSAhAAAAD8BIEBQIEgQAhAAAAD8AJEBSIEkQA5AAAAD8AJEASOAkSA5AAAAD8ApEASICkQA5AAAAD8BJEBSIEkQA5AAAAACAIEBf4EAQAAgAAAACAoECf4CAQAAgAAAH8AQEAgIAggv/hgAAAAAH4AQIAgQEhAf/AQAAAAAf4AQAFAASAAD+AAAAf4EQAFAASAAD+AAAAPwAgQFAgSBAD8AAAAPwCgQJAgKBAD8AAAAPwCgQJAgKBAj8AAAAPwCgQBAgKBAD8AAAAf4EQAFAASAACAAAAAfwEAQUAgQCAH+AAAAfwAAQEAgQCAH+AAAAfwCAQIAgICAn+AAAAfwCAQAAgICAH+AAAAeDADYEBAQMAHgAAAAQAH/gBAkCBwECAAAA"),
+b=atob("AwIGCAgICAMFBQYIAwYDBwcFBgYHBgYGBgYDAwYHBgcHBgYGBgYGBgYEBgYGBgYGBgYGBgYGBggGBgYEBwQGBwQGBgYGBgYHBgYGBgYGBgYGBgYGBgYGBgYGBgQCBAgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABwAHCAYGBgAGBgYGAAYGBQYABgMGBgQABgYHBgAGBgYGBgYGBgYGBgYGBgYEBAYGBgYGBgYGAAYGBgYGBgYHBgYGBgYGBgYGBgYGBgYGBwcGBgYGBgYABgYGBgYGBg==");exports.add=function(c){c.prototype.setFontHaxorNarrow7x17=function(){this.setFontCustom(a,32,b,17)}}});
+Modules.addCached("FontHaxorNarrow7x17",function(){var a=atob("AAAAAAAAAAAB/MAAAJAAcAAAABIADgAAAAACAAngB4AeeAHgB5AAQAAAAHEAREAiID/4CIgERAEcAAAAwMCQgEmAGTADJAISBgYAAAAHgHQgRBAjCA5kAAwAPQAAAkABwAAAAAfADBgIAggAgAACACCAIDBgB8AAAAEgAGAA/AAYABIAAAABAACAAEAB/AAQAAgABAAAAAAkABwAAAAgABAACAAEAAIAAAAADAAGAAAAAIABgAMABgAMABgAAAAB/AEBAQxAjCAgIA/gAAACAAIAAgAB/8AAACBwIMgQhAiCA4EAAACAgIAgQhAhCA94AAAAGAA0ACIAYQB/8ABAAAAPhARBAiCBEECHwAAAH/ARBAiCBEEBHwAAAIAAQPAhgBMADgAAAAHvAQhAhCBCEB7wAAAHhAQhAhCBCEB/wAAADDAGGAAAAYUAwwAAAAQABQAEQAQQBAQAAABEACIAEQAIgARAAiAAAAICAIIAIgAKAAIAAAAIAAgABBMCEAEQAHAAAAAf8BAECOIEiQJEgP4AAAAP8BiAEEAGIAD/AAAB/8CEIEIQIQgPeAAAA/4CAIEAQIAgICAAAB/8CAIEAQEBAH8AAAB/8CEIEIQIQgQBAAAB/8CIAEQAIgAQAAAAA/4CAIEAQIQgI+AAAB/8AEAAIAAQAf/AAABAED/4EAQAAAQGAgCBAECAIH/gAAAf/ABAAFAAxgGAwAAAf/AACAAEAAIAAQAAAf/AYAAMABgAH/wAAAf/AYAAMAAGAH/wAAAP+AgCBAECAID/gAAAf/AhABCACEADwAAAAP+AgCBAUCAQD/QAAAf/AhABCACHADxwAAAOCAiCBCECCICDgAAAQAAgAB/8CAAEAAAAAf+AACAAEAAIH/gAAAfAAB4AAMAHgHwAAAAf8AAGAAwAGAADAABgf8AAABgcAzAAYADMAYHAAABwAAYAAPwBgAcAAAABAMCBoEcQLAgYBAAAD/8EAIIAQAAAYAAMAAGAADAABgAAgAABACCAEH/4AAABAAMAAgAAwAAQAAAAAAgABAACAAEAAIAAQAABAABAABAAAAAATgBIgCRAEkAH8AAAH/wAhACBAECAH4AAAAfgBAgCBAECAEIAAAAfgBAgCBACEB/8AAAAfgBIgCRAEiAHIAAAAgAH/gSAAkABIAAAAAdYBFICKQEUgPJAQMAAAP/gBAAEAAIAAP4AAAAAgCBAn+AAEAAIAAAAAQAAQAAgIBCf8AAAP/gAIAAwAGQAQYAAAAAgQBA/+AAEAAIAAAB/gCAAH+AIAAP4AAAB/gBAAEAAIAAP4AAAA/ACBAECAIEAPwAAAB/4BCAECAIEAPwAAAA/ACBAECAEIAf+AAAB/gBAAEAAIAAIAAAAAxACRAEiAIkAIwAAABAAf+AECAIEAQIAAAB/AABAACAAIAf4AAABwAAcAAGABwAcAAAAB+AADAA4AAMAfgAAABhgAkAAwACQAYYAAAB4MANgAEAAwAeAAAABBgCFAEyAKEAYIAAAOTgjYhABAAAD/4AAAQAQjYg5OAAAAEAAQAAgAAgAAgABAAEAAAAB/gMQAggAxDAf5AACAAAAQB/+AEEAQIAAQAAgAAAXQARABBACCAEEAEQAXQAAAH/4AAQoAhgBAACAAABwQEQQoQiQRAQcAAADYwJIQRIghJAxsAAABwQkQQoQiQRAQcAAABwQEQSIQ0QRQQcAAACAAkAAv/iQAAgAAAAAQYAhQFMgShAGCAAAAQYEhQFMgShAGCAAAAQYAhQFMgChAGCAAADAAJAASAAYAAAAAE4ASKAkaBJEB/AAAABECEIH/wBAgEBAAABAAEAAAAAAAgQBA/+FAEMAIAAAAxAKRAkiAokAIwAAAQAAQABAAAAAAMQEkQFIgSJACMAAAAMQAkSBI0CJQCMAAAAQAH/gBAgCBBUCDAAAAAAgwBCgKZAlCAMEAAAAgwJCgKZAlCAMEAAAAgwBCgKZAFCAMEAAAH/wIQBQgEhwA8cAAAA/wGIBQQEYgAP8AAAA/wmICQQCYgAP8AAAA/xGIBQQEYgAP8AAAA/wmIAQQCYgAP8AAAX/xAAgABAACAAEAAAD/gIAhQBEgCAgIAAAD/gIAkQBogCggIAAAD/hIAhQBEgCAgIAAAH/wIQhQhEhCBAEAAAH/wIQoQhohCRAEAAAH/woQgQhChCBAEAAAH/xIQhQhEhCBAEAAAEAQv/iQBAAAFAET/4UAQAAAf/EgCFAERAQB/AAAAf/AhCBCEBAQB/AAAAf/AYAEMAQGAH/wAAAf/EYAEMAQGAH/wAAAP+AgCFAESAID/gAAAP+CgCJAEKAID/gAAAP+CgCJAEKAIj/gAAAP+CgCBAEKAID/gAAAf/EhAFCASHADxwAAAf+CACKAEIAIH/gAAAf+AACEAEQAIH/gAAAf+CACIAEIAIn/gAAAf+CACAAEIAIH/gAAAcAAGAED8QYAHAAAAAQAAgAR//CAAEAAAAAP/AgABEECIIDoQAPAAAAH+AEABQAEgAAgAAAACcAJEBSIEkgA/gAAACcApECSICkgA/gAAACcBJEBSIEkgA/gAAACcApEASICkgA/gAAAACFAET/4AAQAAgAAAD8AIEBQIEgQAhAAAAD8AIEAQOAgSAhAAAAD8BIEBQIEgQAhAAAAD8AJEBSIEkQA5AAAAD8AJEASOAkSA5AAAAD8ApEASICkQA5AAAAD8BJEBSIEkQA5AAAAACAIEBf4EAQAAgAAAACAoECf4CAQAAgAAAH8AQEAgIAggv/hgAAAAAH4AQIAgQEhAf/AQAAAAAf4AQAFAASAAD+AAAAf4EQAFAASAAD+AAAAPwAgQFAgSBAD8AAAAPwCgQJAgKBAD8AAAAPwCgQJAgKBAj8AAAAPwCgQBAgKBAD8AAAAf4EQAFAASAACAAAAAfwEAQUAgQCAH+AAAAfwAAQEAgQCAH+AAAAfwCAQIAgICAn+AAAAfwCAQAAgICAH+AAAAeDADYEBAQMAHgAAAAQAH/gBAkCBwECAAAA"),
+b=atob("AwIGCAgICAMFBQYIAwYDBwcFBgYHBgYGBgYDAwYHBgcHBgYGBgYGBgYEBgYGBgYGBgYGBgYGBggGBgYEBwQGBwQGBgYGBgYHBgYGBgYGBgYGBgYGBgYGBgYGBgQCBAgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABwAHCAYGBgAGBgYGAAYGBQYABgMGBgQABgYHBgAGBgYGBgYGBgYGBgYGBgYEBAYGBgYGBgYGAAYGBgYGBgYHBgYGBgYGBgYGBgYGBgYGBwcGBgYGBgYABgYGBgYGBg==");exports.add=function(c){c.prototype.setFontHaxorNarrow7x17=function(){this.setFontCustom(a,32,b,17)}}});
+
+
+let BGCOLOR = 0;
+let FGCOLOR = "#FFFFFF";
+
+g.clear();
+
+g.setColor("#808080").fillRect(0,43,239,53).setColor(BGCOLOR);
+for(let x=4; x<240; x+=10) g.fillCircle(x,48,4);
+
+function delaymsXX(ms) {
+  var t = getTime()+ms/1000; while(getTime()<t);
+}
+
+function drawDots(arr, x, y, xrad, yrad, xoff, yoff) {
+  //rad=4;
+  sx=xrad+2, sy=yrad+2;
+  //print(`xoff is ${typeof(xoff)} and ${xoff}`);
+  if(typeof(xoff)==='undefined') 
+    xoff = 0, yoff=0;
+
+  xoff+=x*sx*6, yoff+=y*sy*8;
+  g.setColor(BGCOLOR);
+  g.fillRect(xoff, yoff, xoff+4*sx+xrad, yoff+6*sy+yrad);
+  g.setColor(FGCOLOR);
+  for(let c=0; c< 5; c++) {
+    let col=arr[c];
+    for(let r=0; r<7; r++) {
+      if(col&1) 
+        g.fillRect(xoff+c*sx, yoff+r*sy, xoff+c*sx+xrad, yoff+r*sy+yrad);
+      //g.fillCircle(xoff+c*s+rad/2, yoff+r*s+rad/2, 4); //r/2 is cool!!
+      col >>= 1;
+    }
+  }
+}
+
+function drawDotsSlow(arr, x, y, xrad, yrad, xoff, yoff) {
+  //rad=4;
+  sx=xrad+2, sy=yrad+2;
+  //print(`xoff is ${typeof(xoff)} and ${xoff}`);
+  if(typeof(xoff)==='undefined') 
+    xoff = 0, yoff=0;
+
+  xoff+=x*sx*6, yoff+=y*sy*8;
+  // draw vertical
+  for(let r=0; r<7; r++) {
+      g.setColor(BGCOLOR);
+      g.fillRect(xoff, yoff+r*sy, xoff+4*sx+xrad, yoff+r*sy+yrad);
+    delayms(12);
+    let b=1 << r;
+    for(let c=0; c< 5; c++) {
+      /*
+      if(arr[c] & b) 
+        g.setColor(FGCOLOR);
+      else
+        g.setColor(BGCOLOR);
+      g.fillRect(xoff+c*sx, yoff+r*sy, xoff+c*sx+xrad, yoff+r*sy+yrad);
+      */
+      if(arr[c] & b) { 
+        g.setColor(FGCOLOR);
+        g.fillRect(xoff+c*sx, yoff+r*sy, xoff+c*sx+xrad, yoff+r*sy+yrad);
+      }    
+    }
+  }
+
+}
+
+function drawDigit(d, x, y, xscale, yscale, xoff, yoff) {
+  drawDots(new Uint8Array([
+    62,65,65,65,62, //0
+    0,66,127,64,0, //1
+    98,81,73,73,70, //2
+    34,65,73,73,54, //3
+    24,20,18,127,16, //4
+    79,73,73,73,49, //5
+    62,73,73,73,48, //6
+    1,1,113,9,7, //7
+    54,73,73,73,54, //8
+    6,73,73,73,62, //9
+    0,0,36,0,0, //:
+    32,16,8,4,2, // slash
+  ]).slice(5*d,5*(d+1)), x, y, xscale, yscale, xoff, yoff);
+}
+
+function drawDOW(dow, x, y, xscale, yscale, xoff, yoff) {
+  let dowArr = new Uint8Array([
+    6,73,73,73,48, //S
+    63,64,64,64,63, //U
+    127,2,12,2,127, //M
+    62,65,65,65,62, //O
+    1,1,127,1,1, //T
+    63,64,64,64,63, //U
+    127,32,24,32,127, // W
+    127,73,73,73,65, // E
+    1,1,127,1,1, //T
+    127,8,8,8,127, //H
+    127,9,9,9,1, // F
+    127,9,25,41,70, //R
+    6,73,73,73,48, //S
+    126,9,9,9,126, //A
+    ]);
+  drawDots(dowArr.slice(10*dow,10*dow+5), x, y, xscale, yscale, xoff, yoff);
+  drawDots(dowArr.slice(10*dow+5,10*(dow+1)), x+1, y, xscale, yscale, xoff, yoff);
+}
+
+let lastTime = '';
+let lastDate = '';
+let lastSecs = '';
+
+//g.setBgColor(BGCOLOR).setColor(FGCOLOR).fillCircle(78,112,2).fillCircle(78,140,2);
+
+//require("FontDylex7x13").add(Graphics);
+//g.setFont("Dylex7x13",1).setFontAlign(0,0);
+require("FontHaxorNarrow7x17").add(Graphics);
+g.setFont("HaxorNarrow7x17").setFontAlign(0,0);
+
+function clrDtWdw(r,gr,b) {
+  g.setColor(r,gr,b).fillRect(0,166,239,239).setColor(1,1,1);  
+}
+
+function drawDate(dt) {
+  //let dtstr = dt.toString().substring(0,16);
+  let m = dt.getMonth()+1;
+  let d = dt.getDate();
+  if(lastDate != d) {
+    lastDate = d;
+    let digs = [Math.floor(m / 10), m % 10, 11, Math.floor(d / 10), d % 10];
+    clrDtWdw(0,0,0);
+    FGCOLOR="#60c0c0";
+    for(let d = 0; d < digs.length; d++) {
+      if(!d && !digs[d]) continue;
+      drawDigit(digs[d], [4,5,6,7,8][d], 4, 2, 3, 0, 8);
+    }
+    drawDOW(dt.getDay(), 1, 4, 2, 3, 12, 8);
+  }
+}
+  
+let bx=104, by = 12;
+
+  g.setColor(1,1,1).fillRect(bx,by+1,bx+29,by+14).fillRect(bx+1,by,bx+28,by+15).fillRect(bx+30,by+3,bx+32,by+12);
+
+
+function drawBattery(b) {
+  g.setColor(0,0,0).fillRect(bx+1,by+1,bx+28,by+14);
+  g.setColor(1,1,1);
+  if(b > 25) g.fillRect(bx+2,by+2,bx+9,by+13);
+  if(b > 50) g.fillRect(bx+11,by+2,bx+18,by+13);
+  if(b > 75) g.fillRect(bx+20,by+2,bx+27,by+13);
+}
+
+function drawAmPm(isPM) {
+  let ampm = new Uint8Array([
+    126,9,9,9,126, //A
+    127,2,12,2,127, //M
+    0,0,0,0,0, // space
+    127,9,9,9,6, // P
+//    127,9,25,41,70, //R
+    127,2,12,2,127, //M
+    ]);
+  FGCOLOR = isPM ? '#8080C0' : '#C0C080';
+  drawDots(ampm.slice(10*isPM,10*isPM+5), 10, 3, 1, 1, 2, 0);
+  drawDots(ampm.slice(10*isPM+5,10*isPM+10), 11, 3, 1, 1, 2, 0);
+  drawDots(ampm.slice(10*isPM+10,10*isPM+15), 12, 3, 1, 1, 2, 0);
+}
+
+function showMsg(m) {
+  clrDtWdw(0,0.3,0.3);
+  m.split('|').forEach((s,i)=>{g.drawString(s,120, 174+i*18);});
+  //(m, 120, 186);
+  //lastDate = '';
+  [400,800,1200,2000,2400].forEach((t) => {setTimeout(Bangle.buzz, t, 175);});
+}
+
+let tikint = 0;
+let lastDigs = new Uint8Array([11,11,11,11]);
+
+function clock() {
+  wOS.time_left = 10;
+  let dt = new Date();
+  let tm = dt.toString().substring(16,21);
+  // inelegant, but it's this or split on ' ' and reassemble :-P
+  if(dt.getDate() < 10) tm = dt.toString().substring(15,20);
+  drawDate(dt);
+  let hr = dt.getHours();
+  let ampm = 0;
+  if(hr >= 12) {
+    ampm = 1;
+    hr -= 12;
+  }
+  if(!hr)  hr=12;
+
+  if(tm != lastTime) {
+    lastTime = tm;
+    let digs = new Uint8Array([Math.floor(hr / 10), hr % 10, Math.floor(dt.getMinutes() / 10), dt.getMinutes() % 10]);
+    for(let d = 0; d < digs.length; d++) {
+      if(digs[d] == lastDigs[d]) continue;
+      // only when changing hour
+      if(d == 1) drawAmPm(ampm);
+      if(d < 2) FGCOLOR="#80E0E0"; else FGCOLOR="#FFFFFF";
+      drawDigit(digs[d], [0,1,2,3][d], 1, 5, 8, 4, 0);
+    }
+    lastDigs = digs;
+    tick();
+    drawBattery(E.getBattery());
+    //g.drawString(analogRead(D31).toFixed(4),120, 40, true);
+  }
+  if(! tikint) {
+    tikint = setInterval(tick, 1000);
+  }
+}
+
+function tick() {
+  let s = new Date().getSeconds();
+  if(!s) { clock(); return; }
+  let digs = [Math.floor(s / 10), s % 10];
+  let ldigs = [Math.floor(lastSecs / 10), lastSecs % 10];
+  for(let d = 0; d < digs.length; d++) {
+    if(digs[d] != ldigs[d])
+      drawDigit(digs[d], 0, 0, 3, 5, [184,212][d], 100);
+  }
+  lastSecs = s;
+  if(--wOS.time_left <= 0) {
+    clearInterval(tikint);
+    tikint=0;
+  }
+}
+
+let logD = () => {}; //console.log;
+let alarmTOs = [];
+let schAls = (als) => {
+  for(let idx=0; idx < alarmTOs.length; idx++) {
+    clearTimeout(alarmTOs[idx]);
+  }
+  alarmTOs = [];
+  for(let idx=0; idx < als.length; idx++) {
+    //logD('idx = '+idx);
+    let tdiff = Date.parse(als[idx].time) - Date.now();
+    let msg = als[idx].msg;
+    if(tdiff > 0) {
+      //logD(`will alarm ${msg} in ${tdiff}`);
+      alarmTOs.push(setTimeout(showMsg, tdiff,als[idx].msg));
+    } else {
+      //expired
+      //logD('tossing out' + idx);
+    }
+  }
+};
+
+ACCEL.on("faceup", clock);
+//wOS.wake();  don't... doubles up the timer
+// swipe dn (1) use it to reset/reprint date
+setWatch(clock, BTN1, {edge: "rising", repeat: true});
+TC.on("swipe", (d) => {
+  if(d == 1) lastDate = '';
+  drawDate(new Date());
+});
+/*
+schAls( [
+    {"time":"2022-06-24T10:00:00", "msg":"JUST TESTING|RELAX..."},
+    {"time":"2022-06-24T16:50:00", "msg":"SET UP THE|BBQ"},
+    {"time":"2022-06-24T19:30:00", "msg":"MIRANDA"},
+]);
+*/
+//E.stepCount=()=>{return 0;}
