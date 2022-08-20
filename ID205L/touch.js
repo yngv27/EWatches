49b40496eb0a17038321aaffd6105e7b82d5ff12
JV <61598758+yngv27@users.noreply.github.com>
2022-08-20 16:25:17 -0400

    Create touch.js

---
+++ ID205L/touch.js
@@ -0,0 +1,43 @@
+// Espruino driver for IT7259 touch sensor
+
+const I2C_DEVICEID = 0x46;
+const I2C_REG = 225;
+
+/*
+ * sda: sda I2C pin
+ * scl: scl I2C pin
+ * int: interrupt pin
+ * reset: reset pin, chip is off when low
+ * enable: negated reset pin, chip is on when high
+ */
+function init(cfg) {
+  const i2c = new I2C();
+  i2c.setup({ sda: cfg.sda, scl: cfg.scl });
+
+  const result = {
+    enable: () => {
+      cfg.enable.write(1);
+      cfg.reset && cfg.reset.write(0); // ID205L uses separate reset pin for screen and touch controller
+    },
+    disable: () => cfg.enable.write(0),
+    onTouch: event => { },
+    onSwipe: event => { },
+    read: () => {
+      i2c.writeTo(I2C_DEVICEID, I2C_REG);
+      const res = i2c.readFrom(I2C_DEVICEID, 16);
+      return { x: res[2], y: res[4], type: res[0], dir: (res[10] >> 1) - 3 };
+    }
+  }
+
+  if (cfg.int) {
+    setWatch(
+      () => result.onTouch(result.read()),
+      cfg.int,
+      { edge: 'falling', debounce: 1, repeat: true }
+    );
+  }
+
+  return result;
+}
+
+module.exports = init;
