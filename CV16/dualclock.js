b61fc8fddcd422f3d3a7a3cd8c022b833d19d921
JV <61598758+yngv27@users.noreply.github.com>
2023-01-08 12:34:11 -0500

    Update dualclock.js

--- CV16/dualclock.js
+++ CV16/dualclock.js
@@ -1,26 +1,5 @@
-// calibrate
 
-/*
-let threshold = 0;
-function calibrate() {
-  let calibCnt = 100;
-  let calInt = setInterval(() => {
-    let r = analogRead(D5);
-    if (r > threshold) threshold = r;
-    if(calibCnt-- < 0) clearInterval(calInt);
-  }, 150);
-}
-
-let TGT = "Magic3"; //"TK78G";
-
-let BLE=require("ble_simple_uart");
-NRF.requestDevice( {filters: [{ namePrefix: TGT }] }).then(function(dev) {
-  BLE.write(dev, "D8.reset()\n", function() {
-    print("Started");
-  });
-});
-*/
-
+// for use with setDigitRaw()
 /*
                 0x01
                ======
@@ -47,8 +26,8 @@ function send(a, d) {
   wOS.I2C.writeTo(0x3f,a,d);
   //print(`Sending ${d} to ${a}`);
 }
-let on=() => {send(0, 0xaf);};
-let off=() => {send(0, 0xae);};
+let lcd_on=() => {send(0, 0xaf);};
+let lcd_off=() => {send(0, 0xae);};
 
 
 // send(0, 0xae); 
@@ -130,9 +109,10 @@ function setDigitRaw(idx, val, icon) {
   send(0x40, val & 0xff);
 }
 
+let inMsg = false;
+
 let min10 = 0; // keep track of digit 7 for flashing the colon
 function clock() {
-  g.setColor(.45,.45,.6).fillRect(0,0,239,239);
   let h = Date().getHours(), m = Date().getMinutes();
   //g.drawImage(_S.read("minHand2"), 120, 120, {rotate: Math.PI * m / 30});
   //g.drawImage(_S.read("hrHand2"), 120, 120, {rotate: Math.PI * (h * 60 + m) / 360});
@@ -142,9 +122,11 @@ function clock() {
   setDigit(7, min10, true);
   setDigit(8, m%10);
   // battery
-  let b = E.getBattery();
-  setDigit(3, Math.floor(b/10), true);
-  setDigit(4, b%10);
+  if(!inMsg) {
+    let b = E.getBattery();
+    setDigit(1, Math.floor(b/10), true);
+    setDigit(2, b%10);
+  }
 }
 
 function date() {
@@ -158,9 +140,11 @@ function date() {
 
 lcdInit();
 setEverything('             ');
+/*
 setDigitRaw(0, 0x5450);  //b
 setDigitRaw(1, 0x5050);  //a
 setDigitRaw(2, 0x5440);  //t
+*/
 
 let clkint=setInterval(clock, 60000);
 clock();
@@ -168,7 +152,68 @@ date();
 
 // try this
 setInterval(()=>{
-    setDigit(7, min10, (Date().getSeconds() % 2) ? false : true);
+  setDigit(7, min10, (Date().getSeconds() % 2) ? false : true);
 }, 1000);
 
-setWatch(wOS.wake, BTN2, {repeat: true, edge: "rising"});
+let drawBkgd = () => {
+  g.setColor(.4,.6,.4).fillRect(0,0,239,239);
+  setDigitRaw(0, 0x4441);  //[
+  setDigitRaw(3, 0x55);  //]
+};
+
+setWatch(()=>{
+  if(!wOS.awake) wOS.wake();
+  else {
+    drawBkgd();
+    inMsg = false;
+  }
+}, BTN2, {repeat: true, edge: "rising"});
+
+require("FontDylex7x13").add(Graphics);
+g.setFont("Dylex7x13",2).setFontAlign(0,0);
+
+function showMsg(m) {
+  setEverything('?????'); // clear the bottom field of 5
+  g.setColor(0);
+  m.split('|').forEach((s,i)=>{g.drawString(s,120, 178+i*20);});
+  [400,800,1200,2000,2400].forEach((t) => {setTimeout(Bangle.buzz, t, 175);});
+  inMsg = true;
+}
+let alarmTOs = [];
+let schAls = (als) => {
+  for(let idx=0; idx < alarmTOs.length; idx++) {
+    clearTimeout(alarmTOs[idx]);
+  }
+  alarmTOs = [];
+  for(let idx=0; idx < als.length; idx++) {
+    //logD('idx = '+idx);
+    let tdiff = Date.parse(als[idx].time) - Date.now();
+    let msg = als[idx].msg;
+    if(tdiff > 0) {
+      //logD(`will alarm ${msg} in ${tdiff}`);
+      alarmTOs.push(setTimeout(showMsg, tdiff,als[idx].msg));
+    } else {
+      //expired
+      //logD('tossing out' + idx);
+    }
+  }
+};
+
+LED = {
+  w: (a,d) => { wOS.I2C.writeTo(0x30, a, d);} ,
+  init: () => {
+    LED.w(0, 0x1f);
+    LED.w(9, 0);
+    LED.w(1, 0x10);
+    LED.w(2, 0x68);
+    LED.w(3, 0x18);
+    LED.w(5, 0x31);
+  },
+  breathe: (rgb) => {
+    LED.w(0,0x7);
+    LED.w(9,0x0);
+    LED.w(5,0xff);
+    LED.w(4,0x40 | rgb); // rgb = red * 32 + grn * 8 + blue * 2
+    LED.w(1,0x0f);
+  },
+};
