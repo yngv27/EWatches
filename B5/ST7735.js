9bf95ac55225bcf605008ca094824064c90f4412
yngv27@gmail.com <Anthro29!>
2023-03-31 12:41:07 -0400

    adopting new 2.16 FW

---
+++ B5/ST7735.js
@@ -0,0 +1,65 @@
+function ST7735() {
+    var LCD_WIDTH = 80;
+    var LCD_HEIGHT = 160;
+    var XOFF = 24;
+    var YOFF = 0;
+    var cmd = lcd_spi_unbuf.command;
+  
+    function dispinit(rst,fn) {
+        function delayms(d) {var t = getTime()+d/1000; while(getTime()<t);}
+        if (rst) {
+            digitalPulse(rst,0,10);
+        } else {
+            cmd(0x01); //ST7735_SWRESET: Software reset, 0 args, w/delay: 150 ms delay
+        }
+    setTimeout(function() {
+      cmd(0x11); //ST7735_SLPOUT: Out of sleep mode, 0 args, w/delay: 500 ms delay
+      setTimeout(function() {
+        cmd(0xB1,[0x01,0x2C,0x2D]); //ST7735_FRMCTR1: Set Frame rate ctrl - normal mode, 3 args: (Rate = fosc/(1x2+40) * (LINE+2C+2D))
+        cmd(0xB2,[0x01,0x2C,0x2D]); //ST7735_FRMCTR2: Set Frame rate control - idle mode, 3 args: Rate = fosc/(1x2+40) * (LINE+2C+2D)
+        cmd(0xB3,[0x01,0x2C,0x2D,0x01,0x2C,0x2D]); //ST7735_FRMCTR3: Set Frame rate ctrl - partial mode, 6 args: Dot inversion mode + Line inversion mode
+        cmd(0xB4,0x07); // ST7735_INVCTR: Set Display inversion ctrl, 1 arg, no delay: No inversion
+        cmd(0xC0,[0xA2,0x02,0x84]); //ST7735_PWCTR1: Set Power control, 3 args, no delay: init + -4.6V + AUTO mode
+        cmd(0xC1,0xC5); //ST7735_PWCTR2: Set Power control, 1 arg, no delay: VGH25 = 2.4C VGSEL = -10 VGH = 3 * AVDD
+        cmd(0xC2,[0x0A,0x00]); //ST7735_PWCTR3: Set Power control, 2 args, no delay: Opamp current small + Boost frequency
+        cmd(0xC3,[0x8A,0x2A]); //ST7735_PWCTR4: Set Power control, 2 args, no delay: BCLK/2, Opamp current small & Medium low
+        cmd(0xC4,[0x8A,0xEE]); //ST7735_PWCTR5: Set Power control, 2 args, no delay:
+        cmd(0xC5,0x0E); //ST7735_VMCTR1: Set Power control, 1 arg, no delay:
+        cmd(0x20,0x00); //ST7735_INVOFF: Don't invert display, no args, no delay
+        cmd(0x36,0xC8); //ST7735_MADCTL: Set Memory access control (directions), 1 arg: row addr/col addr, bottom to top refresh
+        cmd(0x3A,0x05); //ST7735_COLMOD: Set color mode, 1 arg, no delay: 16-bit color
+        cmd(0x2A,[0x00,0x00,0x00,LCD_WIDTH-1]); //ST7735_CASET: Set Column addr set, 4 args, no delay: XSTART = 0 + XEND = 127
+        cmd(0x2B,[0x00,0x00,0x00,LCD_HEIGHT-1]); //ST7735_RASET: Set Row addr set, 4 args, no delay: XSTART = 0 + XEND = 127
+        cmd(0xE0,[0x02,0x1c,0x07,0x12,0x37,0x32,0x29,0x2d,0x29,0x25,0x2B,0x39,0x00,0x01,0x03,0x10]); //ST7735_GMCTRP1: color and gamma correction
+        cmd(0xE1,[0x03,0x1d,0x07,0x06,0x2E,0x2C,0x29,0x2D,0x2E,0x2E,0x37,0x3F,0x00,0x00,0x02,0x10]); //ST7735_GMCTRN1: color and gamma correction
+        cmd(0x13); //ST7735_NORON: Set Normal display on, no args, w/delay: 10 ms delay
+        cmd(0x29); //ST7735_DISPON: Set Main screen turn on, no args w/delay: 100 ms delay
+  
+        if (fn) fn();
+      },10);
+    } ,100);
+  
+  }
+  
+    function connect(options , callback) {
+        var spi=options.spi, dc=options.dc, ce=options.cs, rst=options.rst;
+        var g = lcd_spi_unbuf.connect(options.spi, {
+            dc: options.dc,
+            cs: options.cs,
+            height: LCD_HEIGHT,
+            width: LCD_WIDTH,
+            colstart: XOFF,
+            rowstart: YOFF
+        });
+        g.lcd_sleep = function(){cmd(0x10);cmd(0x28);};
+        g.lcd_wake = function(){cmd(0x29);cmd(0x11);};
+        dispinit(rst, ()=>{g.clear(1).setFont("6x8").drawString("Loading...",20,20);});
+        return g;
+    }
+  
+    //var spi = new SPI();
+    SPI1.setup({sck:D2, mosi:D3, baud: 8000000});
+  
+    return connect({spi:SPI1, dc:D6, cs:D5, rst:D8});
+  }
+  
---
+++ B5/apwB5.js
@@ -0,0 +1,461 @@
+Modules.addCached("FontDylex7x13",function(){var a=atob("AAAAAAAAA/QAAcAAAHAAAAJAf4CQH+AkAAAMQJIP+CSBGAAAQAUIEYAwBiBCgAgAAG4EiCRA0gBgDIAAOAAAAfAwYgCAAIAjBgfAAACgAgB8AIAKAAABAAgB8AIAEAAAACAOAAAIAEACABAAgAAADAAAAYAwBgDAGAAAAfgQIJkECB+AAAIQIIP8ACABAAAQwQoIkEiBhAAAQgQIJEEiBuAAADACgCQCID/ACAAAeQJEEiCRBHAAAHwFEEiCRAHAAAQAIMEYCwBgAAANwJEEiCRA3AAAOAIkESCKA+AAAGYAAAAgzgAACACgCICCAAAKAFACgBQAoAAAggIgCgAgAABABAAjQSAGAAAA8AhAmQUoL0CKA4AAABwHAMgGQA4ADgAAf4JEEiCRA4gDgAADwCECBBAggQIQAAH+CBBAggQIQDwAAD/BIgkQSIJEECAAB/gkASAJAEAAAAeAQgQIIkESBOAAA/wCABAAgAQB/gAAQIP8ECAAABAAQQIIEH8AAB/gEADACQCECBAAA/wAIAEACABAAA/wMABgAwBgB/gAAf4MABgAMABg/wAADwCECBBAgQgHgAAH+CIBEAiAOAAAB4BCBAggQIQD2AAD/BEAiARgHIACAAAxAkQSIIkESBGAAAgAQAIAH+CABAAgAAAP4ACABAAgAQfwAAHAAcABgAwDgOAAAD4ADgGAMABgAOD4AAAwwEgBgAwAkBhgAAYACAAgAPAIAIAYAAAEGCFBEgkQUIMEAAH/yAJAEAAYADAAYADAAYAAQBIAn/wAAGAMAYADAAYAAAAIAEACABAAgAQAIAAQAEAAAADAKQFICkA+AAD/gIQEICEA8AAAPAIQEICEAkAAAPAIQEICEP+AAAPAKQFICkA0AAAQA/wkASAIAAAAPAISEJCEh/gAD/gIAEACAA+AAAQBPwAAABAAggSfwAA/4AQAYASAQgAAgAf8AAA/AQAIAD4CABAAfAAAPwEACABAAfAAAHgEICEBCAeAAAP+EICEBCAeAAAHgEICEBCA/4AAPwCACABAAQAAAEQFICkBKAiAAAIAfwCEBCABAAAPgAIAEACA/AAAMABgAMAYAwAAAPAAYAYAwAGABgPAAACEAkAMAJAIQAAD5ACQBIAkP8AACEBGAlAUgMQAAAgAQD3iAJAEAAf/AAEASAI94BAAgAAAIAIAEADAAgAQAQAAAFAHwFUCqBBARAAAACAOAAAAQQI/4kASAAAADgAAA4AAAEAAABAAAAQAAEACAH/AgAQAAAFACgH/AoAUAAAEAEAEABAAQAAAGMAYAwBjAAAAwAADEKRDIiiQRIEYAAAIAKAIgAAH4ECCBA/AkQSIIEAACDFChiRSIKEGCAADAAQAAAEAMAAADAAQAwAEAAABADAAQAwAAAAQAcAfAHABAAAAQAIAEACABAAAAQAIAEACABAAgAQAAAgAgAIAIAAACAB4AgAAAPAGADwAAAEQlIKkJKAiAAAIgCgAgAAAeAQgIQDwCkBSAaAAAIQkYKUJSAxAAAYACAQgAOEIAIAYAAAL8AAAeAQgf4EIBIAAATA+gkQSIAEAABBAfAIgEQCIB8BBAAAwAEgBQAeAUASAwAAAffAADCCYhKQjIIYAAEAAABAAAAH4ECCZBSgpQQIH4AAAQBUAqAPAAAAQAUAVAFAEQAAAQAIAEADwAAH4ECC9BUglQQIH4AAIAEACABAAgAQAIAAAAwAkASAGAAAAIgEQPoBEAiAACIBMAqAJAAAEQCoBUAUAAAEAEAAAAAEH8AIACABAfAAQAAGAHgD/hAA/4QAAAA4AcAOAAAAFADAACQD4AEAAAOAIgEQBwAAAEQBQBUAUAEAAA8YAwBkDGGHgAgAAeMAYAwBpjFQBIAAIgFTB2AMgYww8AEAAADACQWIAEAEAAADgOBJAUgBwAHAAABwHAUgSQA4ADgAAA4TgSQJICcABwAAAcJwJICkCOAA4AAAOE4AkASAnAAcAAAHDcCSBJAbgAOAAADgGANAIgH+CRBAgAAHgEIECSBxAgQgAAH8SSFJAkgQQAAH8CSFJEkgQQAAH8KSJJCkgQQAAH8KSBJCkgQQAAEET+FBAAAQQv4kEAAFBE/hQQAAUED+FBAAACAP4EkCSBBARAHAAAH8KAIwCGCAwP4AAA4AiEghQQEQBwAAAcARBQRIICIA4AAAOBIhIIkEJEAcAAAHAkQkEKCIiAOAAADgSICCBBCRAHAAACIAoAIAKAIgAAD0CECNBYgQgXgAAD8ABEAhAQAIH4AAB+AAhARAIAED8AAA/BARAIgEICB+AAAfggIAEACEBA/AAAMABAAQEHEEAEAMAAAH+AkASAJADAAAABD/CQhIQkINEAcAAADAKQlIKkA+AAADAKQVISkA+AAADAqQlIKkA+AAADAqQlIKkI+AAADAqQFIKkA+AAADBKRVISkA+AAADAKQFIB8BSApANAAADwCEhDghAJAAADwSkFSApANAAADwKkJSApANAAADwKkJSCpANAAADwKkBSCpANAAAkAL8AACgCfgAAUAT8EAAACQAPwgAAAAcERCogkQvwAAF+EgBQBIAD4AAA8EhBQgIQDwAAA8AhBQhIQDwAAA8ChCQgoQDwAAA8ChCQgoQjwAAA8ChAQgoQDwAAAQAIAVACABAAAA9AjAWgMQLwAAB8EBBAgAQH4AAB8CBCAgAQH4AAB8CBCAggQH4AAB8CBAAggQH4AAB8ABJAlASH+AAH/ghAQgIQDwAAB8CBIAkgSH+AAA"),
+b=atob("AwIEBgYIBwIEBAYGAwYCBgYGBgYHBgYGBgYCAwUGBQYIBwcHBwcGBwcEBgcGBwcHBgcHBwgHBwgHCAcEBgQGCAMGBgYGBgYGBgMFBgMIBgYGBgYGBgYGCAYGBgYCBggABwADBgQGBgYGBwcECAAHAAADAwUFBgYIBQgGBAgABggAAgYGCAgCBgQIBQYFAAgIBQYFBQMIBwQDBAUGBwcIBgcHBwcHBwgHBgYGBgQEBAQIBwcHBwcHBgcHBwcHCAYIBgYGBgYGCAYGBgYGAwMEBAYGBgYGBgYGBgYGBgYGBgY=");exports.add=function(c){c.prototype.setFontDylex7x13=function(){this.setFontCustom(a,32,b,13)}}});
+const _Storage = require('Storage');
+eval(_Storage.read("B5.driver.js"));
+setTimeout(ACCEL.init, 2000);
+g.sc = g.setColor;
+
+// chill out that green
+pal[2] = 0x050;
+pal[3] = 0x005;
+
+require("FontDylex7x13").add(Graphics);
+g.setFont("Dylex7x13");
+
+let logD = (msg) => { console.log(msg); };
+
+
+/*
+** BEGIN WATCH FACE
+*/
+let cy = (m) => {
+ return Math.floor(Math.cos(Math.PI * m/30) * -80);
+};
+
+let cx = (m) => {
+return Math.floor(Math.sin(Math.PI * m/30) * 80);
+};
+               
+let xS=.8,yS=.8;
+const startX=[10,45,10,45],startY=[18,18,80,80],nmX=[16,42,88,126],nmY=[12,12,12,12];let rotate=!1;function setScale(t,r){xS=t,yS=r}function drawScaledPoly(r,n,a){let d=[];for(let t=0;t<r.length;t+=2){var e;d[t]=Math.floor(r[t]*xS)+n,d[t+1]=Math.floor(r[t+1]*yS)+a,rotate&&(e=d[t],d[t]=80-d[t+1],d[t+1]=e)}g.fillPoly(d,!1)}let d0=new Uint8Array([0,4,4,0,32,0,36,4,36,65,25,65,25,5,11,5,11,65,36,65,36,66,32,70,4,70,0,66]),d1=new Uint8Array([7,4,11,0,20,0,24,4,24,70,13,70,13,5,7,5]),d2=new Uint8Array([0,4,4,0,32,0,36,4,36,34,32,38,11,38,11,65,36,65,36,66,32,70,0,70,0,36,4,32,25,32,25,5,0,5]),d3=new Uint8Array([0,4,4,0,32,0,36,4,36,66,32,70,4,70,0,66,0,65,25,65,25,38,0,38,0,32,25,32,25,5,0,5]),d4=new Uint8Array([0,4,4,0,11,0,11,32,25,32,25,0,32,0,36,4,36,70,25,70,25,38,0,38]),d5=new Uint8Array([0,0,32,0,36,4,36,5,11,5,11,32,32,32,36,36,36,66,32,70,4,70,0,66,0,65,25,65,25,38,0,38]),d6=new Uint8Array([0,4,4,0,32,0,36,4,36,5,11,5,11,65,25,65,25,38,11,38,11,32,32,32,36,36,36,66,32,70,4,70,0,66]),d7=new Uint8Array([0,4,4,0,32,0,36,4,36,70,25,70,25,5,0,5]),d8=new Uint8Array([0,4,4,0,32,0,36,4,36,32,33,35,36,38,36,66,32,70,18,70,18,65,25,65,25,38,18,38,18,32,25,32,25,5,11,5,11,32,18,32,18,38,11,38,11,65,18,65,18,70,4,70,0,66,0,38,3,35,0,32]),d9=new Uint8Array([0,4,4,0,32,0,36,4,36,66,32,70,4,70,0,65,0,65,25,65,25,5,11,5,11,32,25,32,25,38,4,38,0,34]);function drawDigit(t,r,n){let a=(n?nmX:startX)[t];t=(n?nmY:startY)[t];EMULATOR&&(a+=80),drawScaledPoly([d0,d1,d2,d3,d4,d5,d6,d7,d8,d9][r],a,t)}
+
+let drawMin = (m) => {
+  g.setColor(7);
+  for(let a=0; a<=m; a++) {
+    g.drawLine(40,80,40+cx(a),80+cy(a));
+    //g.flip();
+  }
+  g.flip();
+};
+function pct2col(r,g,b,p) {
+  let r1 = Math.floor(r*32*p);
+  if(r1 > 31) r1 = 31;
+    let g1 = Math.floor(g*32*p);
+  if(g1 > 31) g1 = 31;
+    let b1 = Math.floor(b*32*p);
+  if(b1 > 31) b1 = 31;
+  //r1 *= p; g1 *= p; b1 *= p;
+  return (r1 << 11) + (g1 << 6) + b1;
+}
+
+/*
+** END WATCH FACE
+*/
+var volts;
+var batt=battInfo();
+let lastTime = '';
+let EMULATOR = false; 
+let showClockTO = 0;
+
+let myName = NRF.getAddress().slice(-5);
+console.log(myName);
+
+/**** BEGIN ALARMS *******/
+let _Alarms = [];
+let inAlarm = false;
+let loadAlarms = () => {
+  _Alarms =  _Storage.readJSON('alarms.json');
+  if(!_Alarms) _Alarms = [{"msg":"13:53|Stop|working|today","time":"2021-12-21T13:53:00"}];
+  //_Alarms.sort((a,b) => {return(a.time > b.time);});
+  scheduleAlarms();
+};
+
+function vibrat(lvl, cnt, onms, offms) {
+  let tout = 0;
+  digitalWrite(VIB, 0);
+  for(let x=0; x < cnt; x++) {
+    setTimeout(analogWrite, tout, VIB, lvl);
+    tout += onms;
+    setTimeout(analogWrite, tout, VIB, 0);
+    tout += offms;
+  }
+  
+}
+function notify() {
+  logD('notify START');
+  vibrat(0.5, 3, 300, 150);
+  logD('notify END');
+}
+
+function buzz() {
+  vibrat(0.5, 1, 250, 100);
+}
+
+let showMsg = (title, msg) => {
+  logD('showMsg START');
+  inAlarm = true;
+  
+  //g.setFont("Dylex7x13");
+  
+  g.sc(3);
+  g.fillRect(0,0,79,159);
+  g.sc(15);
+  g.setFontAlign(0,-1);
+  let y = 8 +  g.getFontHeight();
+  if(title) {
+    g.drawString('=== '+title+' ===', 40, y);
+    g.drawString('=== '+title+' ===', 41, y);
+    y += g.getFontHeight();
+    logD(`t:${title}`);
+  }
+  let lines = msg.split("|");
+  for(let l = 0; l < lines.length; l++) {
+    g.drawString(lines[l], 40, y);
+    logD(`l:${lines[l]}`);
+    y += g.getFontHeight();
+  }
+
+  notify();
+  logD('showMsg END');
+};
+
+/*
+** alarms are in order, pop the top and show it
+*/
+
+function showAlarm(msg) {
+  console.log(`alarming w ${msg}`);
+  showMsg('ALARM', msg);
+  // remove the TO from the list, so we don't kill something by accident
+}
+
+let alarmTOs = [];
+let scheduleAlarms = () => {
+  for(let idx=0; idx < alarmTOs.length; idx++) {
+    clearTimeout(alarmTOs[idx]);
+  }
+  alarmTOs = [];
+  for(let idx=0; idx < _Alarms.length; idx++) {
+    logD('idx = '+idx);
+    let tdiff = Date.parse(_Alarms[idx].time) - Date.now();
+    let msg = _Alarms[idx].msg;
+    if(tdiff > 0) {
+      logD(`will alarm ${msg} in ${tdiff}`);
+      alarmTOs.push(setTimeout(showAlarm, tdiff,_Alarms[idx].msg));
+    } else {
+      //expired
+      logD('tossing out' + idx);
+    }
+  }
+};
+//loadAlarms();
+var la = loadAlarms;
+let schAls = (als) => {_Alarms = als; scheduleAlarms(); };
+
+/*
+********************************************* END ALARMS *******************************
+*/
+
+/*
+var buzzLock = 0;
+function buzzClock (h,m) {
+  // skip if either lockout or canceled: 10 or 01 (i.e. not 0)
+  if(buzzLock) {
+    buzzLock &= 0b10;
+    console.log('no buzz..resetting');
+    return;
+  }
+  console.log('buzzing');
+  // vibrate: long = 5, short = 1
+  const lvl = 0.8, LONGBZ = 600, SHORTBZ = 150;
+  // hours
+  let n = Math.floor(h/5);
+  if(n) vibrate(lvl, n, LONGBZ, 100);
+  vibrate(lvl, h%5, SHORTBZ, 200);
+  // delay
+  vibrate(0.0, 1, 500, 500);
+  // 10 mins - always single pulses
+  n = Math.floor(m/10);
+  if(n) vibrate(lvl, n, SHORTBZ, 200);
+  vibrate(0.0, 1, 500, 500);
+  // 1 mins
+  if(m % 10 >= 5){ vibrate(lvl, 1, LONGBZ, 100); m -= 5; }
+  vibrate(lvl, m%5, SHORTBZ, 200);
+  // lockout for one minute
+  buzzLock |= 0b10;
+  setTimeout(function() { buzzLock &= 0b01; digitalWrite(VIB, 0);}, 60000);
+}
+*/
+ACCEL.isFaceUp = false;
+
+function checkFaceup() {
+  let xyz = ACCEL.read();
+  //console.log(JSON.stringify(xyz));
+  if((xyz.ax > 20 && xyz.ax < 240) || xyz.ay > 40 || xyz.ay < 20) {
+    ACCEL.isFaceUp = false;
+    return;
+  }
+  // have we switched?
+  if(ACCEL.isFaceUp == false) {
+    ACCEL.emit("faceup");
+  }
+  ACCEL.isFaceUp = true;
+}
+
+ACCEL.on("faceup", ()=>{
+  logD(`** FACE UP! **`);
+  if(g.isOn) return;
+  drawDayClock();
+  setTimeout(()=>{g.off();logD('**OFF**');}, 7500);
+});
+
+function drawDayClock() {
+  g.on();
+  //logD('checkClock START');
+  let d=Date();
+  let dt=d.toString().substr(0,10);
+  d=d.toString().split(' ');
+  let tm=d[4].substring(0,5);
+  let hr=d[4].substr(0,2);
+  let min=d[4].substr(3,2);
+  //let sec=d[4].substr(6,2);
+
+  if (tm == lastTime) {
+    logD(`clock unchanged - returning`);
+    return;
+  }
+  lastTime = tm;
+
+  logD(`tm = ${tm}; lastTime = ${lastTime}`);
+  
+  if(inAlarm) {
+    g.setBgColor(0);
+    g.drawString(`  ${tm}  `, 40, 4, false);
+    g.drawString(`  ${tm}  `, 41, 4, true).flip();
+    return;
+  }
+
+
+  let nm = false;
+  
+  if(hr > 20 || hr < 8) {
+    nm = true;
+    g.setBrightness(10);
+  } else if( hr > 9 && hr < 18) {
+    g.setBrightness(255);
+  } else {
+    g.setBrightness(64);
+  }
+  
+  hr %= 12;
+  if (hr === 0) hr = 12;
+  min = parseInt(min);
+  xmid = g.getWidth()/2;
+
+  g.clear();
+  console.log("DrawClock: time to draw");
+
+  rotate = false;
+  /* 4 digits in corners
+  g.sc(8+3);
+  //g.sc(5);
+  if(EMULATOR) g.setColor(1,1,1);
+  drawDigit(0,Math.floor(hr/10), false);
+  drawDigit(1,Math.floor(hr%10), false);
+  g.flip();
+  g.sc(8+7);
+  //g.sc(8+5);
+  drawDigit(2,Math.floor(min/10), false);
+  drawDigit(3,Math.floor(min%10), false);
+  */
+  /* centered hour with sweeping ticks 
+  drawMin(min);
+
+  g.sc(0); g.fillCircle(40,80,28);
+  g.sc(15);
+  if(Math.floor(hr/10) > 0) {
+    drawScaledPoly(d1,14,52);
+    drawScaledPoly([d0,d1,d2,d3,d4,d5,d6,d7,d8,d9][hr%10],38,52);
+  } else {
+    drawScaledPoly([d0,d1,d2,d3,d4,d5,d6,d7,d8,d9][hr],28,52);
+  }
+  g.flip();
+  */
+  const darr = [d0,d1,d2,d3,d4,d5,d6,d7,d8,d9];
+  //hr = 12;
+  //min = 34;
+  /* silly animation
+  g.setColor(8);
+  for(let r=10; r < 40; r+=4) {
+    g.drawCircle(40,80,r);
+    g.flip();
+  }
+  g.setColor(7);
+  g.drawCircle(40+cx(min)/2, 80+cy(min)/2, 4);
+  */
+  
+  g.sc(11);
+  if(Math.floor(hr/10) > 0) {
+    setScale(0.6,0.8);
+    drawScaledPoly(d1,0,52);
+    drawScaledPoly(darr[hr%10],18,52);
+  } else {
+    setScale(0.8,0.8);
+    drawScaledPoly(darr[hr],8,52);
+  }
+  g.sc(15);
+  setScale(0.5,0.55);
+  drawScaledPoly(darr[Math.floor(min/10)], 42,52);
+  drawScaledPoly(darr[Math.floor(min%10)], 62,52);
+  g.flip();
+
+    // TOP BAR
+  //g.setFont("Dylex7x13");
+  g.setFontAlign(0,-1);
+  g.sc(2);
+  let batt = battInfo(); //process.env.VERSION; //battInfo();
+  // full bkgd
+  //g.fillPoly([0,0,8,12,71,12,79,0],true);
+  // par'l bkgd
+  g.fillPoly([0,0,8,8,71,8,79,0],true);
+  g.drawString(batt,xmid,1);
+  g.sc(10);
+  //g.setClipRect(0,0,79,7)
+  g.drawString(batt,xmid,1);
+//g.setClipRect(0,0,79,159);
+
+  
+  // BOTTOM BAR 
+  g.sc(1);
+  // 1
+  g.fillPoly([0,159,8,151,71,151,79,159],true);
+  //g.fillRect(0,140,79,159);
+  g.sc(11);
+  g.drawString(dt,xmid,143);
+
+  g.flip();
+}
+
+function drawNightClock(d) {
+  g.clear();
+    g.on();
+    rotate = true;
+    g.setColor(4);
+    if(EMULATOR) g.setColor(0.5,0.5,0.5);
+  //console.log("draw1: "+d.hr);
+    if (d.hr>9) drawDigit(0,Math.floor(d.hr/10), true);
+    drawDigit(1,Math.floor(d.hr%10), true);
+  //console.log("draw2: "+d.min);
+    drawDigit(2,Math.floor(d.min/10), true);
+    drawDigit(3,Math.floor(d.min%10), true);
+    g.fillCircle(40, 80,2);
+    g.fillCircle(24, 80,2);
+    let b = battLevel();
+    for(let c=0; c<5; c++) {
+      if(b > c*20) g.drawCircle(16+12*c, 8, 4);
+      //else g.drawCircle(14+12*c,8,4);
+    }
+    g.flip();
+}
+
+function clock(){
+  volts=0;
+  showClockTO = 0;
+  return setInterval(checkFaceup,555);
+}
+
+function sleep(){
+  g.clear();//g.flip();
+  g.off();
+  inAlarm = false;
+  // clean up the last screen's mess...
+  if(showClockTO)   clearTimeout(showClockTO);
+  // just in case - shutdown the buzzer
+  analogWrite(VIB,0);
+  //currscr=-1;
+  return 0;
+}
+
+var screens=[clock,sleep];
+var currscr= 0;
+var currint=screens[currscr]();
+//let longpress = 0;
+let longpressTO = 0;
+
+let nextScreen = () => {
+  currscr++;if (currscr>=screens.length) currscr=0;
+  if (currint > 0) clearInterval(currint);
+  logD(`Running screen ${currscr}`);
+  currint = screens[currscr]();
+  buzz();
+};
+/* */
+
+
+let BUTTON = {
+  lastUp: 0,
+  longpressTO: 0,
+  tapTO: 0,
+  longTime: 1000,
+  tapTime: 250,
+  dbltap: false,
+  watchUp: false,
+  upOpts: { repeat:false, edge:'falling', debounce:25},
+  dnOpts: { repeat:false, edge:'rising', debounce:25},
+};
+
+  
+const btnDown = (b) => {
+  //longpress = b.time;
+  if(BUTTON.tapTO) {
+    clearTimeout(BUTTON.tapTO);
+    BUTTON.tapTO = 0;
+    BUTTON.dbltap = true;
+  }
+  BUTTON.longpressTO = setTimeout(function(){
+    // long press behaviour
+    BUTTON.emit('longpress');
+    BUTTON.longpressTO = 0;
+    // ignore button up
+    BUTTON.watchUp = false;
+  }, BUTTON.longTime);
+  logD(`lpto=${BUTTON.longpressTO}`);
+  BUTTON.watchUp = true;
+  setWatch(btnUp, BTN1, BUTTON.upOpts);
+};
+
+const btnUp = (b) => {
+  if(BUTTON.longpressTO) {
+    clearTimeout(BUTTON.longpressTO);
+    BUTTON.longpressTO = 0;
+  } 
+  if(BUTTON.dbltap) {
+    BUTTON.emit('dbltap');
+    BUTTON.dbltap = false;
+  } else if (BUTTON.watchUp) {
+    BUTTON.tapTO = setTimeout(function(){
+      // long press behaviour
+      BUTTON.emit('tap');
+      BUTTON.tapTO = 0;
+      BUTTON.dbltap = false;
+    }, BUTTON.tapTime);
+    logD(`lpto=${BUTTON.tapTO}`);
+  }
+  BUTTON.lastUp = b.time;
+  setWatch(btnDown, BTN1, BUTTON.downOpts);
+};
+
+setWatch(btnDown, BTN1, BUTTON.downOpts);
+BUTTON.on('tap',()=>{console.log('tap');});
+BUTTON.on('longpress',()=>{
+  console.log('longpress');
+  if(g.isOn) {
+    inAlarm = false;lastTime='';
+    buzz();
+  }
+});
+BUTTON.on('dbltap',()=>{
+  console.log('dbltap');
+  nextScreen();
+});
+Bangle = { buzz:buzz};
