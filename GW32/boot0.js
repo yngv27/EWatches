05223c46fcdf155a99c5e09b99c9324d177f8698
yngv27@gmail.com <Anthro29!>
2024-07-19 15:22:02 -0400

    added DST to startup

--- GW32/boot0.js
+++ GW32/boot0.js
@@ -7,3 +7,18 @@ E.enableWatchdog(20, false);
 
 let _S = require("Storage");
 if(_S.read("GW32.js")) eval(_S.read("GW32.js"));
+
+E.showMessage = function(msg,title) {};
+()=> {
+  let dt=Date();
+  let m = dt.getMonth(), d=dt.getDate(), dow=dt.getDay();
+  if(m<2 || m>10) E.setTimeZone(-5);
+  if(m>2 && m<10) E.setTimeZone(-4);
+  if(m == 2) {
+    if(d-dow > 7) E.setTimeZone(-4);
+    else E.setTimeZone(-5);
+  } else if (m == 10) {
+    if(d-dow > 0) E.setTimeZone(-5);
+    else E.setTimeZone(-4);
+  }
+};//();
