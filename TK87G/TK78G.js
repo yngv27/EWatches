95b7ce011819a23d729e7174fe8da237f31d856c
yngv27@gmail.com <Anthro29!>
2024-12-22 10:54:42 -0500

    working drivers

--- TK87G/TK78G.js
+++ TK87G/TK78G.js
@@ -14,8 +14,47 @@ wOS = {
     analogWrite(wOS.BUZ, 0.5); setTimeout(()=>{wOS.BUZ.reset();}, ms);
   },
   isCharging: ()=>{return !(wOS.CHG.read());},
-  
-  setLCDBrightness: ()=>{},
+  BKL: D8,
+  sleep: ()=> {
+      g.lcd_sleep();
+      TC.stop();
+      wOS.setLCDBrightness(0);
+      wOS.emit("sleep");
+      wOS.isAwake = false;
+  },
+  wake: ()=>{
+      wOS.ticker = 7;
+      if(wOS.isAwake) return;
+      TC.start();
+      g.lcd_wake();
+      wOS.setLCDBrightness(wOS.brightLevel());
+      if(!wOS.tikint) wOS.tikint = setInterval(wOS.tick, 1000);
+      wOS.emit("wake");
+      wOS.isAwake = true;
+    },
+    tick: ()=>{
+      wOS.ticker--;
+      wOS.emit("tick");
+      if(wOS.ticker <= 0){
+        clearInterval(wOS.tikint);
+        wOS.tikint = 0;
+        wOS.sleep();
+      }
+    },
+    brightLevel: ()=> { 
+      let c=Math.floor(Date().getHours()/3);
+      return [0.1,0.5,0.7,0.99][c > 3 ? 7-c : c]; 
+    },
+    setLCDBrightness: (lvl)=>{analogWrite(wOS.BKL, lvl);},
+    setLCDPower:(b)=>{
+      if(b){
+        if(wOS.isAwake) wOS.time_left=wOS.ON_TIME;
+        else wOS.wake();
+      }else wOS.sleep();
+    },
+    isLCDOn:()=>{
+      return wOS.isAwake;
+    }
 };
 
 wOS.BUZ.reset(); // in case we go nuts on start up
@@ -36,11 +75,14 @@ setWatch(()=>{wOS.buzz();}, wOS.CHG, {"edge":"both", "repeat":true});
 Bangle = wOS;
 wOS.UI = {};
 logD = ()=>{};
-// we're "special"
-//NRF.setAdvertising({},{name:"MagicX "+NRF.getAddress().split(':').slice(-2).join('')});
 
-// battery is D3, hi=0.223 lo=0.20
-E.getBattery = () => { return (analogRead(wOS.BAT)-0.2)*3333; };
+// battery is D3, hi=0.23 lo=0.20
+E.getBattery = () => {
+  let batLo = 0.20 , batHi = 0.23;
+  let pct = Math.floor((analogRead(wOS.BAT) - batLo) * 100 / (batHi-batLo));
+  return (pct > 100) ? 100 : pct;
+};
+
 setInterval(()=>{
   NRF.setAdvertising({
     0x180F : [E.getBattery()] // Service data 0x180F = 95
@@ -48,6 +90,21 @@ setInterval(()=>{
 }, 300000);
 wOS.setStepCount = (n) => {};
 wOS.getStepCount = () => { return 0; };
+
+SPI1.setup({sck:D4, mosi:D26, baud: 8000000});
+g=require("~ST7789.js").connect({spi:SPI1, dc:D27, cs:D5, rst:D6, height:280, yoff:20, inverse:1, MADCTL:0});
+
+wOS.I2C= new I2C();
+wOS.I2C.setup({sda: D38, scl: D37, bitrate: 100000});
+var ACCEL=require("~ACCEL.js").connect({i2c: wOS.I2C});
+ACCEL.init();
+ACCEL.on("faceup", wOS.wake);
+
+//TC = {start: ()=>{}, stop: ()=>{}};
+wOS.I2C2 = new I2C();
+wOS.I2C2.setup({sda: D33, scl: D34, bitrate: 200000});
+var TC = require("~CSTx16.js").connect({i2c: wOS.I2C2, rst: D35, intr: D36}); //TK78G
+
 
 // MANAGE EVENTS
 let BUTTON = {
@@ -58,8 +115,8 @@ let BUTTON = {
   tapTime: 250,
   dbltap: false,
   watchUp: false,
-  dnOpts: { repeat:false, edge:'falling', debounce:25},
-  upOpts: { repeat:false, edge:'rising', debounce:25},
+  upOpts: { repeat:false, edge:'falling', debounce:25},
+  dnOpts: { repeat:false, edge:'rising', debounce:25},
 };
   
 const btnDown = (b) => {
